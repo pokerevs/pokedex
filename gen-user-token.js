9df21dfb45ff4db14c6599323b3757114d6b5cca
Erin <sylphofelectricity@gmail.com>
2016-07-14 19:32:02 -0500

    make user token gen take username from cmdline args

--- gen-user-token.js
+++ gen-user-token.js
@@ -1,35 +1,33 @@
 const jwt = require('jsonwebtoken'),
-	  mongoose = require('mongoose'),
-	  readline = require('readline');
+	  mongoose = require('mongoose');
 
 const config = require('./config'),
 	  User = require('./app/models/user');
+
+if (process.argv.slice(2).length == 0) {
+	console.log('usage: node gen-user-token.js <username>');
+	process.exit(1);
+}
 
 mongoose.connect(config.mongoUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 	console.log(`Connected to mongo database at ${config.mongoUrl}`)
+	
+	const username = process.argv.slice(2)[0];
+	User.findOne({'username': username}).exec((err, doc) => {
+		if (err) {
+			console.log(`Database error finding user ${username}: ${err}`);
+			process.exit(1);
+		}
+		if (!doc) {
+			console.log(`Could not find user ${username}`);
+			process.exit(1);
+		}
 
-	const rl = readline.createInterface({
-	  input: process.stdin,
-	  output: process.stdout
-	});
-
-	rl.question('Enter username to generate token for: ', (username) => {
-		User.findOne({'username': username}).exec((err, doc) => {
-			if (err) {
-				console.log(`Database error finding user ${username}: ${err}`);
-				process.exit(1);
-			}
-			if (!doc) {
-				console.log(`Could not find user ${username}`);
-				process.exit(1);
-			}
-
-			const token = jwt.sign({id: doc._id}, config.jwtSecret);
-			console.log(`Token is ${token}`);
-			db.close();
-		});
+		const token = jwt.sign({id: doc._id}, config.jwtSecret);
+		console.log(`Token for ${username}: ${token}`);
+		db.close();
 	});
 });
